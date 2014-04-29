83b1e6be1ec45f359b5f986f84526acf2b6550f8
623637646 <623637646@qq.com>
2014-04-29 18:08:08 +0800

    主页tabbar样式修改

--- Code Prometheus/CPTabBarController.m
+++ Code Prometheus/CPTabBarController.m
@@ -17,20 +17,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.tabBar setTintColor:[UIColor whiteColor]];
-    // 人脉
-    [self.tabBar.items[0] setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_CONTACTS_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_CONTACTS]];
-    ((UITabBarItem*)self.tabBar.items[0]).imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    // 日程
-    [self.tabBar.items[1] setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_SCHEDULE_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_SCHEDULE]];
-    ((UITabBarItem*)self.tabBar.items[1]).imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    // 地图
-    [self.tabBar.items[2] setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_MAP_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_MAP]];
-    ((UITabBarItem*)self.tabBar.items[2]).imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    // 工具
-    [self.tabBar.items[3] setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_TOOLS_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_TOOLS]];
-    ((UITabBarItem*)self.tabBar.items[3]).imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-
+//    [self.tabBar setTintColor:[UIColor whiteColor]];
+    [self.tabBar.items enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UITabBarItem* item = obj;
+        [item setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], UITextAttributeTextColor, nil] forState:UIControlStateSelected];
+//        item.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+        switch (idx) {
+            case 0:
+                // 人脉
+                [item setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_CONTACTS_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_CONTACTS]];
+                item.title = @"首页";
+                break;
+            case 1:
+                // 日程
+                [item setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_SCHEDULE_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_SCHEDULE]];
+                item.title = @"日程";
+                break;
+            case 2:
+                // 地图
+                [item setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_MAP_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_MAP]];
+                item.title = @"地图";
+                break;
+            case 3:
+                // 工具
+                [item setFinishedSelectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_TOOLS_H] withFinishedUnselectedImage:[UIImage imageNamed:CP_RESOURCE_IMAGE_TAB_TOOLS]];
+                item.title = @"更多";
+                break;
+            default:
+                break;
+        }
+    }];
     if (CP_IS_IOS7_AND_UP) {
         
     }else{
