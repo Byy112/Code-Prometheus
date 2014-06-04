ffc30c712bd35b17f65ca1af8e085ddf539e6e8d
623637646 <623637646@qq.com>
2014-06-04 12:00:18 +0800

    返回，添加，编辑，保存，取消按钮样式修改

--- Code Prometheus/CPNavigationController.m
+++ Code Prometheus/CPNavigationController.m
@@ -7,8 +7,11 @@
 //
 
 #import "CPNavigationController.h"
+#import <NYXImagesKit.h>
 
-@interface CPNavigationController ()
+#define CP_NAV_IMAGE_SIZE CGSizeMake(24,24)
+
+@interface CPNavigationController ()<UINavigationControllerDelegate>
 
 @end
 
@@ -31,6 +34,61 @@
     else
     {
         self.navigationBar.tintColor = [UIColor colorWithRed:0.0/255.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0];
+    }
+    
+    if (CP_IS_IOS7_AND_UP) {
+        [[UIBarButtonItem appearance] setBackButtonBackgroundImage:[[UIImage imageNamed:@"cp_back_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    }
+    
+    self.delegate = self;
+    
+}
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    if (CP_IS_IOS7_AND_UP) {
+        viewController.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+    }else{
+        viewController.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"cp_back_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] style:UIBarButtonItemStylePlain target:nil action:nil];
+    }
+
+    if (viewController.navigationItem.leftBarButtonItem) {
+        [self changeStyleWithLeft:YES navig:viewController.navigationItem];
+    }
+    
+    if (viewController.navigationItem.rightBarButtonItem) {
+        [self changeStyleWithLeft:NO navig:viewController.navigationItem];
+    }
+}
+
+-(void) changeStyleWithLeft:(BOOL)left navig:(UINavigationItem*) navig{
+    UIBarButtonItem* old = left ? navig.leftBarButtonItem : navig.rightBarButtonItem;
+    
+    NSNumber *value = [old valueForKey:@"systemItem"];
+    UIBarButtonSystemItem style = [value integerValue];
+    
+    UIBarButtonItem* new = nil;
+    switch (style) {
+        case UIBarButtonSystemItemAdd:
+            new = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"cp_add_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] style:UIBarButtonItemStylePlain target:old.target action:old.action];
+            break;
+        case UIBarButtonSystemItemEdit:
+            new = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"cp_edit_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] style:UIBarButtonItemStylePlain target:old.target action:old.action];
+            break;
+        case UIBarButtonSystemItemSave:
+            new = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"cp_save_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] style:UIBarButtonItemStylePlain target:old.target action:old.action];
+            break;
+        case UIBarButtonSystemItemCancel:
+            new = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"cp_cancel_normal"] scaleToSize:CP_NAV_IMAGE_SIZE] style:UIBarButtonItemStylePlain target:old.target action:old.action];
+            break;
+        default:
+            break;
+    }
+    if (new) {
+        if (left) {
+            navig.leftBarButtonItem = new;
+        }else{
+            navig.rightBarButtonItem = new;
+        }
     }
 }
 @end
